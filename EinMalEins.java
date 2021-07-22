f71f86a5a6d028eef27f63892795b810ef9df2f3
BigBodyBugatti <84071231+BigBodyBugatti@users.noreply.github.com>
2021-07-22 11:58:36 +0200

    Add files via upload

---
+++ EinMalEins.java
@@ -0,0 +1,26 @@
+public class EinMalEins {
+
+
+        public static void main(String[] args) {
+
+
+            for (int i = 1; i <= 10; i++) {
+                for (int j = 1; j <= 10; j++) {
+                    System.out.println(i + " x " + j + " = " + (i * j));
+                }
+                // Achtet auf das <=10 sonst geht euer Kleines EinMalEins nur bis
+                // 9x9
+                // Zeilenumbruch damit es schöner aussieht ;-)
+                System.out.println("");
+            }
+        }
+
+
+
+        }
+
+
+
+
+
+
